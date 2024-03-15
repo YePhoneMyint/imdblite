c37e8f396033631512763731fade9c2cda59aeae
Ye Phone Myint <yephonemyint.dev@gmail.com>
2024-03-15 14:47:31 +0630

    create about page

---
+++ app/about/page.tsx
@@ -0,0 +1,42 @@
+const Page = () => {
+  return (
+    <div className="mx-auto max-w-6xl space-y-5 p-3">
+      <h1 className="text-2xl">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ornare aenean
+        euismod elementum nisi quis eleifend quam adipiscing. Lorem ipsum dolor
+        sit amet consectetur adipiscing elit. Sit amet dictum sit amet. Diam vel
+        quam elementum pulvinar etiam non quam. Duis at tellus at urna
+        condimentum mattis. Eu sem integer vitae justo eget magna fermentum
+        iaculis. Nunc vel risus commodo viverra maecenas accumsan lacus vel
+        facilisis. Morbi tristique senectus et netus et malesuada fames ac.
+        Ornare aenean euismod elementum nisi quis eleifend quam.
+      </p>{" "}
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ornare aenean
+        euismod elementum nisi quis eleifend quam adipiscing. Lorem ipsum dolor
+        sit amet consectetur adipiscing elit. Sit amet dictum sit amet. Diam vel
+        quam elementum pulvinar etiam non quam. Duis at tellus at urna
+        condimentum mattis. Eu sem integer vitae justo eget magna fermentum
+        iaculis. Nunc vel risus commodo viverra maecenas accumsan lacus vel
+        facilisis. Morbi tristique senectus et netus et malesuada fames ac.
+        Ornare aenean euismod elementum nisi quis eleifend quam.
+      </p>{" "}
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ornare aenean
+        euismod elementum nisi quis eleifend quam adipiscing. Lorem ipsum dolor
+        sit amet consectetur adipiscing elit. Sit amet dictum sit amet. Diam vel
+        quam elementum pulvinar etiam non quam. Duis at tellus at urna
+        condimentum mattis. Eu sem integer vitae justo eget magna fermentum
+        iaculis. Nunc vel risus commodo viverra maecenas accumsan lacus vel
+        facilisis. Morbi tristique senectus et netus et malesuada fames ac.
+        Ornare aenean euismod elementum nisi quis eleifend quam.
+      </p>
+    </div>
+  );
+};
+
+export default Page;
