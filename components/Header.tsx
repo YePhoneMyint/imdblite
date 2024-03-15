3a85d04d5d2f76365e25a3bf03cc10647d5a0118
Ye Phone Myint <yephonemyint.dev@gmail.com>
2024-03-15 09:49:54 +0630

    add dark mode

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,23 @@
 import Link from "next/link";
 import MenuItem from "./MenuItem";
-import { Home, Info } from "lucide-react";
+import { HomeIcon, InfoCircledIcon } from "@radix-ui/react-icons";
+import { ModeToggle } from "./ModeToggle";
 
 const Header = () => {
   return (
-    <div className="flex justify-between p-3 max-w-6xl mx-auto items-center">
+    <div className="mx-auto flex max-w-6xl items-center justify-between p-3">
       <div className="flex gap-5">
-        <MenuItem title="home" address="/" Icon={Home} />
-        <MenuItem title="about" address="/about" Icon={Info} />
+        <MenuItem title="home" address="/" Icon={HomeIcon} />
+        <MenuItem title="about" address="/about" Icon={InfoCircledIcon} />
       </div>
-      <Link href="/" className="flex gap-1 items-center">
-        <span className="text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg">
-          IMDb
-        </span>
-        <span className="text-xl hidden sm:block">Lite</span>
+      <Link href="/" className="flex items-center gap-3">
+        <ModeToggle />
+        <div className="flex items-center gap-1">
+          <span className="rounded-lg bg-amber-500 px-2 py-1 text-2xl font-bold">
+            IMDb
+          </span>
+          <span className="hidden text-xl sm:block">Lite</span>
+        </div>
       </Link>
     </div>
   );
--- components/MenuItem.tsx
+++ components/MenuItem.tsx
@@ -1,17 +1,19 @@
+import { IconProps } from "@radix-ui/react-icons/dist/types";
 import { LucideIcon } from "lucide-react";
 import Link from "next/link";
+import { ForwardRefExoticComponent, RefAttributes } from "react";
 
 interface Prop {
   title: string;
   address: string;
-  Icon: LucideIcon;
+  Icon: ForwardRefExoticComponent<IconProps & RefAttributes<SVGSVGElement>>;
 }
 
 const MenuItem = ({ title, address, Icon }: Prop) => {
   return (
     <Link href={address} className="hover:text-amber-500">
-      <Icon className="text-2xl sm:hidden" />
-      <p className="uppercase hidden sm:block text-sm">{title}</p>
+      <Icon className="h-6 w-6 sm:hidden" />
+      <p className="hidden text-sm uppercase sm:block">{title}</p>
     </Link>
   );
 };
